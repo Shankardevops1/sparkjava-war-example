0abda498542535b81c55415ad01bfcab0b098ecf
shankar <shankargajula53@gmail.com>
2022-07-13 19:20:41 +0530

    changes

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -2,5 +2,5 @@ def checkout_git() {
     
     echo "this is checking out git code."
           git credentialsId: 'first', url: 'git@github.com:Shankardevops1/sparkjava-war-example.git'
-
+}
 return this
