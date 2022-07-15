34d221433f5b3700d62b207fc158911e83768289
shankar <shankargajula53@gmail.com>
2022-07-15 11:37:51 +0530

    cd

--- vars/create_tag.groovy
+++ vars/create_tag.groovy
@@ -1,10 +1,10 @@
- 4.	def create_tag(String tag) {
-5.	    
-6.	    sh """    
-7.	    git log -1
-8.	    git tag -a ${tag} -m "this tag has been created from jenkins"
-9.	    git push origin ${tag}    
-10.	       """
-11.	}
-12.	
-13.	return this
+ 	def create_tag(String tag) {
+	    
+	    sh """    
+	    git log -1
+	    git tag -a ${tag} -m "this tag has been created from jenkins"
+	    git push origin ${tag}    
+	       """
+	}
+	
+	return this
