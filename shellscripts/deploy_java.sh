10b83b0000ad0c441ac32b29fc5804c291d1427a
shankar <shankargajula53@gmail.com>
2022-07-16 18:46:48 +0530

    jj

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -4,9 +4,9 @@ env=$2
 
 IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 
-mkdir /var/lib/jenkins/shankar
-cd /var/lib/jenkins/shankar
-aws s3 cp s3://jenkins-test-88/java-code4/$tag/sparkjava-hello-world-1.0.war .
+mkdir /var/lib/jenkins/deploy
+cd /var/lib/jenkins/doploy
+aws s3 cp s3://jenkins-test-88/java_code2/$tag/sparkjava-hello-world-1.0.war .
 scp sparkjava-hello-world-1.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.64/webapps
 ssh ec2-user@${IP} << HERE
     rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.64/work
