89de958b612f731bfdcbee635fcb67321f5179ea
shankar <shankargajula53@gmail.com>
2022-07-16 18:34:45 +0530

    cga

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -4,8 +4,8 @@ env=$2
 
 IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 
-mkdir /var/lib/jenkins/deploy
-cd /var/lib/jenkins/deploy
+mkdir /var/lib/jenkins/practice
+cd /var/lib/jenkins/practice
 aws s3 cp s3://jenkins-test-88/java_code4/$tag/sparkjava-war-example.war .
 scp sparkjava-war-example.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.64/webapps
 ssh ec2-user@${IP} << HERE
